6033fa41589e33c34edd8e573962f743c0674e35
invor <michael.becher.1990@googlemail.com>
2013-01-31 02:05:27 +0100

    Phong shader in/out/uniform variables

--- space-lion/src/shader/f_phong.glsl
+++ space-lion/src/shader/f_phong.glsl
@@ -1,7 +1,23 @@
 //	temporary default fragment shader
 #version 420
 
+uniform sampler2d diffuseMap;
+uniform sampler2d specularMap;
+uniform sampler2d normalMap;
+
+in vec3 vPosition;
+in vec3 vNormal;
+in vec3 vTangent;
+in vec3 vColour;
+in vec2 vUVCoord;
+
 out vec4 fragColour;
+
+vec3 phongShading(in vec3 colour, in vec3 normal, in vec3 lightPosition, in vec3 lightColour , in vec3 viewerPosition)
+{
+	//	placeholder return
+	return colour;
+}
 
 void main()
 {
--- space-lion/src/shader/v_phong.glsl
+++ space-lion/src/shader/v_phong.glsl
@@ -3,9 +3,25 @@
 
 uniform mat4 modelViewProjectionMatrix;
 
-in vec3 vertexPosition;
+in vec3 in_vPosition;
+in vec3 in_vNormal;
+in vec3 in_vTangent;
+in vec3 in_vColour;
+in vec2 in_vUVCoord;
+
+out vec3 vPosition;
+out vec3 vNormal;
+out vec3 vTangent;
+out vec3 vColour;
+out vec2 vUVCoord;
 
 void main()
 {
-	gl_Position = modelViewProjectionMatrix * vec4(vertexPosition, 1);
+	vPosition = modelViewProjectionMatrix * vec4(in_vPosition, 1);
+	vNormal = in_vNormal;
+	vTangent = in_vTangent;
+	vColour = in_vColour;
+	vUVCoord = in_vUVCoord;
+	
+	gl_Position = vPosition;
 }
