5083b27ed5d153768ec7bc30a435a600df5a80ff
invor <michael.becher.1990@googlemail.com>
2020-05-03 17:35:56 +0200

    Working on input handling design

--- src/EngineCore/InputEvent.hpp
+++ src/EngineCore/InputEvent.hpp
@@ -7,46 +7,89 @@ namespace EngineCore
 {
     namespace Common
     {
+        namespace Input
+        {
+
+        }
 
         /**
          * Self-contained defintion for all sorts of hardware input events that acts as intermediate layer between specific definitions
          * of window/context management and the (game) logic part.
-         * Should be created by window system (e.g. glfw) and put into a queue.
          */
         struct InputEvent
         {
             /**
              * Specifies type of event, e.g. key is pressed/released to held down
              */
-            enum EventType { PRESS = 1, RELEASE = 2, HOLD = 4 };
+            enum EventTrigger { PRESS = 1, RELEASE = 2, HOLD = 3, MOVE = 4};
 
             /**
-             * Specifies hardware part that triggered the event, e.g. key, mouse button, joystick axis, etc.
+             * Specifies hardware part that triggered the event, e.g. key, mouse button, gamepad button etc.
              */
-            enum EventTrigger 
+            enum EventElement
             {
 
-
             };
+
+            typedef float ElementState;
 
             InputEvent();
             ~InputEvent();
 
+            std::tuple<EventElement, EventTrigger, ElementState> m_event_conditions;
+
         };
+
 
         /**
          * Matches input events to actions (callbacks) that are to be peformed in response to the event.
-         * The (game) logic part should register these into some sort of map datastructure.
+         * The (game) logic part should register these as part of an input context into some sort of map datastructure.
+         * Use for actions triggered by a single, specific event such as a single key press
          */
-        class InputAction
+        struct InputEventAction
         {
-        public:
-            InputAction();
-            ~InputAction();
+            InputEventAction();
+            ~InputEventAction();
 
-        private:
-            InputEvent            m_event;
-            std::function<void()> m_action;
+            InputEvent                             m_event;
+            std::function<void(InputEvent const&)> m_action;
+        };
+
+
+        /**
+         * Self-contained defintion for input state of one or several devices and keys/buttons/axes.
+         */
+        struct InputState
+        {
+            enum EventDevice { KEYBOARD, MOUSE_BUTTON, MOUSE_AXIS, GAMEPAD_BUTTON, GAMEPAD_AXIS };
+
+            enum EventElement
+            {
+
+            };
+
+        };
+
+        /**
+         * Matches (more complex) input states to actions (callbacks) that are to be performed if the given state persists.
+         */
+        struct InputStateAction
+        {
+            InputState                             m_event;
+            std::function<void(InputState const&)> m_action;
+        };
+
+
+        /**
+         * A combination of several event-driven and state-driven input actions.
+         * Allows to define and switch between different controls, e.g. camera movement, player movement, etc.
+         * Input events should check with all active input contexts for matching event actions,
+         * while once a frame the input system should iterate all state actions for every active context.
+         */
+        struct InputActionContext
+        {
+            std::vector<InputEventAction> m_event_actions;
+            std::vector<InputStateAction> m_state_actions;
         };
 
     }
