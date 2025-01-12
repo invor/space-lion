cbaefa9f618dfeb0432b4f67ea9df2e906ef148e
invor <1233213+invor@users.noreply.github.com>
2025-01-12 03:27:23 +0100

    Update cooldown trigger component

--- src/EngineCore/CooldownTriggerComponentManager.hpp
+++ src/EngineCore/CooldownTriggerComponentManager.hpp
@@ -3,45 +3,32 @@
 
 #include "functional"
 
-#include "BaseMultiInstanceComponentManager.hpp"
+#include "BaseMultiInstanceComponentManager2.hpp"
 #include "ComponentStorage.hpp"
 
 namespace EngineCore {
     namespace Common {
 
-        class CooldownTriggerComponentManager : public BaseMultiInstanceComponentManager
+        struct CooldownTriggerComponentData
         {
-        private:
-            struct Data
-            {
-                Entity                entity;            ///< entity that owns the component
-                bool                  is_active;         ///< flag denoting if cooldown actively counts down
-                float                 remaining_time;    ///< remaining cooldown time
-                float                 reset_time;        ///< value that the cooldown is reset to
-                std::function<void()> cooldown_callback; ///< function that is called when the cooldown time has passed
-            };
+            Entity                entity;            ///< entity that owns the component
+            bool                  is_active;         ///< flag denoting if cooldown actively counts down
+            float                 remaining_time;    ///< remaining cooldown time
+            float                 reset_time;        ///< value that the cooldown is reset to
+            std::function<void()> cooldown_callback; ///< function that is called when the cooldown time has passed
+        };
 
-            Utility::ComponentStorage<Data, 1000, 1000> data_;
-
+        class CooldownTriggerComponentManager : public BaseMultiInstanceComponentManager2<CooldownTriggerComponentData, 1000, 1000>
+        {
         public:
             CooldownTriggerComponentManager() = default;
             ~CooldownTriggerComponentManager() = default;
 
             size_t addComponent(Entity entity, float reset_time, std::function<void()> cooldown_callback, bool start_on_creation = true);
 
-            void deleteComponent(Entity entity);
-
             void resetCooldown(Entity entity);
 
             void resetCooldown(size_t index);
-
-            size_t getComponentCount() const;
-
-            bool checkComponent(size_t index);
-
-            Data const& getComponent(size_t index) const;
-
-            Data& getComponent(size_t index);
         };
 
     }
