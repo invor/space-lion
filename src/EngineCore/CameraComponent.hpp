141d265564777943826df70de328e749f9763b84
invor <1233213+invor@users.noreply.github.com>
2025-02-10 01:03:46 +0100

    Use new base class for bbox and camera components

--- src/EngineCore/CameraComponent.hpp
+++ src/EngineCore/CameraComponent.hpp
@@ -4,53 +4,39 @@
 // forward declartion of Entity
 struct Entity;
 // space-lion includes
-#include "BaseMultiInstanceComponentManager.hpp"
+#include "BaseMultiInstanceComponentManager2.hpp"
 #include "types.hpp"
 
 namespace EngineCore
 {
     namespace Graphics
     {
-        class CameraComponentManager : public BaseMultiInstanceComponentManager
+        struct CameraComponentData {
+            Entity entity;            ///< entity owning the component
+            float  near_cp;           ///< near clipping plane
+            float  far_cp;            ///< far clipping plane
+            float  fovy;              ///< camera vertical field of view in radian
+            float  aspect_ratio;      ///< camera aspect ratio
+            float  exposure;          ///< camera exposure, a simplification of iso-value, aperature number and exposure time
+            Mat4x4 projection_matrix; ///< camera projection matrix
+        };
+
+        class CameraComponentManager : public BaseMultiInstanceComponentManager2<CameraComponentData, 1000, 1000>
         {
         private:
-            struct Data
-            {
-                uint     used;      ///< number of components currently in use
-                uint     allocated; ///< number of components that the allocated memery can hold
-                uint8_t* buffer;    ///< raw data pointer
-
-                Entity* entity;            ///< entity owning the component
-                float* near_cp;            ///< near clipping plane
-                float* far_cp;             ///< far clipping plane
-                float* fovy;               ///< camera vertical field of view in radian
-                float* aspect_ratio;       ///< camera aspect ratio
-                float* exposure;           ///< camera exposure, a simplification of iso-value, aperature number and exposure time
-                Mat4x4* projection_matrix; ///< camera projection matrix
-            };
-
-            Data m_data;
-            mutable std::shared_mutex m_data_access_mutex;
-
             // TODO move this elsewhere?
             Entity m_active_camera;
 
         public:
-            CameraComponentManager(uint size);
-            ~CameraComponentManager();
+            CameraComponentManager();
+            ~CameraComponentManager() = default;
 
-            void reallocate(uint size);
-
-            void addComponent(Entity entity,
+            size_t addComponent(Entity entity,
                 float near_cp = 0.001f,
                 float far_cp = 1000.0f,
                 float fovy = 0.5236f,
                 float aspect_ratio = 16.0 / 9.0f,
                 float exposure = 0.000036f); // default value for mapping avg luminance of ~5000cd/m^2 to 0.18 intensity
-
-            void deleteComponent(Entity entity);
-
-            bool checkComponent(uint entity_id) const;
 
             void setActiveCamera(Entity entity);
 
