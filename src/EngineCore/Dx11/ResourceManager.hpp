531f569b73eb2462ceb263e4fca15497d7ea00f5
invor <1233213+invor@users.noreply.github.com>
2025-03-20 12:06:20 +0100

    Update ResourceManager.hpp

--- src/EngineCore/Dx11/ResourceManager.hpp
+++ src/EngineCore/Dx11/ResourceManager.hpp
@@ -85,6 +85,11 @@ namespace EngineCore
                 ResourceID createStructuredBufferAsync(
                     std::string const& name,
                     std::shared_ptr<std::vector<BufferDataType>> const& data);
+
+                template<typename BufferDataType>
+                void updateStructuredBufferAsync(
+                    ResourceID rsrc_id,
+                    std::shared_ptr<std::vector<BufferDataType>> const& data);
     #pragma endregion
 
     #pragma region Create mesh
@@ -620,9 +625,6 @@ namespace EngineCore
 
                 m_renderThread_tasks.push(
                     [this,idx,data]() {
-
-
-
                         D3D11_BUFFER_DESC desc = { 0 };
                         desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
                         desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
@@ -645,18 +647,49 @@ namespace EngineCore
                     }
                 );
 
-                //auto result = std::async([this, idx, data, desc, shdr_rsrc_view]() {
-                //
-                //    //this->m_textures_2d[idx].resource = std::make_unique<Texture2D>(
-                //    //    m_device_resources->GetD3DDevice(),
-                //    //    *data,
-                //    //    desc,
-                //    //    shdr_rsrc_view);
-                //    //
-                //    this->m_textures_2d[idx].state = READY;
-                //});
-
                 return m_buffers[idx].id;
+            }
+
+            template<typename BufferDataType>
+            inline void ResourceManager::updateStructuredBufferAsync(
+                ResourceID rsrc_id,
+                std::shared_ptr<std::vector<BufferDataType>> const& data)
+            {
+                std::unique_lock<std::shared_mutex> lock(m_buffers_mutex);
+
+                auto query = m_id_to_buffer_idx.find(rsrc_id.value());
+
+                if (query != m_id_to_buffer_idx.end())
+                {
+                    auto idx = query->second;
+
+                    m_renderThread_tasks.push(
+                        [this, idx, data]() {
+                            D3D11_BUFFER_DESC desc = { 0 };
+                            desc.CPUAccessFlags = D3D11_CPU_ACCESS_WRITE;
+                            desc.BindFlags = D3D11_BIND_SHADER_RESOURCE;
+                            desc.ByteWidth = static_cast<UINT>(sizeof(BufferDataType) * data->size());
+                            desc.MiscFlags = D3D11_RESOURCE_MISC_BUFFER_STRUCTURED;
+                            desc.StructureByteStride = sizeof(BufferDataType);
+
+                            D3D11_SHADER_RESOURCE_VIEW_DESC shdr_rsrc_view_desc = { 0 };
+                            shdr_rsrc_view_desc.Format = DXGI_FORMAT_UNKNOWN;
+                            shdr_rsrc_view_desc.ViewDimension = D3D11_SRV_DIMENSION_BUFFER;
+                            shdr_rsrc_view_desc.Buffer.NumElements = static_cast<UINT>(data->size());
+
+                            std::unique_lock<std::shared_mutex> lock(m_buffers_mutex);
+
+                            //TODO check if this becomes dangerous if buffer is in use as a weak resource
+                            this->m_buffers[idx].resource = std::make_unique<dxowl::Buffer>(
+                                m_d3d11_device,
+                                desc,
+                                shdr_rsrc_view_desc,
+                                *(data));
+
+                            this->m_buffers[idx].state = READY;
+                        }
+                    );
+                }
             }
 
             template<typename VertexContainer, typename IndexContainer>
