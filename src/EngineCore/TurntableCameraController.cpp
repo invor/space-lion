30d2b590f04066d5c4ede8b6da5d90da361ec930
becherml <michael.becher@visus.uni-stuttgart.de>
2025-05-24 12:47:02 +0200

    Add panning with left stick

--- src/EngineCore/TurntableCameraController.cpp
+++ src/EngineCore/TurntableCameraController.cpp
@@ -68,6 +68,14 @@ void Editor::Controls::TurntableCameraController::controlCameraGamepadAction(
     // first hardware part is left x axis
     if (std::sqrt(std::pow(std::abs(states[0]), 2.0f) + std::pow(std::abs(states[1]), 2.0f)) > dead_zone)
     {
+        float remapped_state_x = (states[0] - dead_zone) / (1.0f - dead_zone);
+        float remapped_state_y = (states[1] - dead_zone) / (1.0f - dead_zone);
+
+        float factor = 100.0f;
+
+        cam_position += cam_right * factor * remapped_state_x * static_cast<float>(dt);
+        cam_position += cam_up * factor * remapped_state_y * static_cast<float>(dt);
+        transform_mngr.setPosition(camera_transform_idx, cam_position);
     }
 
     if (std::sqrt(std::pow(std::abs(states[2]), 2.0f) + std::pow(std::abs(states[3]), 2.0f)) > dead_zone)
@@ -78,16 +86,24 @@ void Editor::Controls::TurntableCameraController::controlCameraGamepadAction(
         float remapped_state_x = (states[2] - sign_flip_x * dead_zone) / (1.0f - dead_zone);
         float remapped_state_y = (states[3] - sign_flip_y * dead_zone) / (1.0f - dead_zone);
 
-        float factor = 90.0f;
+        float factor_x = 90.0f;
+        float factor_y = 90.0f;
+
+        if (dot(cam_forward, Vec3(0.0, 1.0, 0.0)) < -0.99f) {
+            remapped_state_y = std::min(remapped_state_y, 0.0f);
+        }
+        else if (dot(cam_forward, Vec3(0.0, 1.0, 0.0)) > 0.99f) {
+            remapped_state_y = std::max(remapped_state_y, 0.0f);
+        }
 
         // rotate horizontally
-        auto rot_lon = glm::angleAxis(factor* remapped_state_x * static_cast<float>(dt) * (3.14159265f / 180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
+        auto rot_lon = glm::angleAxis(factor_x * remapped_state_x * static_cast<float>(dt) * (3.14159265f / 180.0f), glm::vec3(0.0f, 1.0f, 0.0f));
         cam_right = glm::rotate(rot_lon, cam_right);
         cam_forward = glm::rotate(rot_lon, cam_forward);
         cam_up = glm::rotate(rot_lon, cam_up);
 
         // rotate vertically
-        auto rot_lat = glm::angleAxis(factor * remapped_state_y * static_cast<float>(dt) * (3.14159265f / 180.0f), -cam_right);
+        auto rot_lat = glm::angleAxis(factor_y * remapped_state_y * static_cast<float>(dt) * (3.14159265f / 180.0f), -cam_right);
         cam_forward = glm::rotate(rot_lat, cam_forward);
         cam_up = glm::rotate(rot_lat, cam_up);
 
@@ -110,6 +126,11 @@ void Editor::Controls::TurntableCameraController::controlCameraGamepadAction(
         auto v = glm::normalize(rot_center_ - cam_position);
 
         auto altitude = glm::length(rot_center_ - cam_position);
+
+        if (altitude > 4000.0)
+        {
+            dz = std::min(dz, 0.0f);
+        }
 
         cam_position = cam_position - (v * dz * (altitude / 50.0f));
         transform_mngr.setPosition(camera_transform_idx, cam_position);
